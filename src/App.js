f0ce4f95f904235851cc4a742a33d8238f4ca8fb
Matt Bentrovato <mbentrovato@atb.com>
2019-02-14 18:07:07 -0700

    Added Delete functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
+import AddNinja from './AddNinja';
 
 class App extends Component {
   state = {
     ninjas : [
       { name: 'Ryu', age: 30, belt: 'Black', id: 1},
       { name: 'Ken', age: 31, belt: 'Black', id: 2},
-      { name: 'Saitama', age: 26, belt: 'Yellow', id: 3}
+      { name: 'Saitama', age: 19, belt: 'Yellow', id: 3}
     ]
+  }
+
+  addNinja = (ninja) => {
+    //Add a key to the ninjas
+    ninja.id = Math.random();
+    //Spread operator [...]
+    //let is used to provide a variable only in the block of code it's in
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas //take the state and assign the new ninjas array properties to it
+    })
+  }
+
+  deleteNinja = (id) => {
+//The filter() method creates an array filled with all array elements that pass a test (provided as a function).    
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id //if ninja.id is equal to current id, return false, and it gets filtered from the array into a new array
+    });
+    this.setState({
+      ninjas: ninjas
+    })
   }
 
   render() {    //class based componented require render method, functional ones do not
@@ -15,7 +37,8 @@ class App extends Component {
       <div className="App">      
         <h1>My first React app</h1>
         <p>Welcome!</p>
-        <Ninjas ninjas={ this.state.ninjas }/>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={ this.state.ninjas } />
+        <AddNinja addNinja={ this.addNinja }/>
       </div>
     );
   }
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -2,15 +2,16 @@ import React from 'react';
 
 //inline destructuring
 // const Ninjas = (props) => {
-const Ninjas = ({ninjas}) => {
+const Ninjas = ({ninjas, deleteNinja}) => {
   //Take original array 'ninjas', we 'map' through the array to recieve the individual 'ninja' which represents the individual items in the array
   //Then we perform a function on each item, which returns a bit of JSX that we want to output for each ninja
   const ninjaList = ninjas.map(ninja => {
     return(
-      <div className="ninja" key={ ninja.id }>
+      <div className="ninja" key={ ninja.id }>       
         <div>Name: { ninja.name }</div>
         <div>Age: { ninja.age }</div>
         <div>Belt: { ninja.belt }</div>
+        <button onClick={() => {deleteNinja(ninja.id)}}>Delete</button> 
       </div>                
     )
   })
